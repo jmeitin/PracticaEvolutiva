28925415b39629b23f79e83d17980b11b20f3722
Javier Meitín Moreno <jmeitin@ucm.es>
2023-02-09 12:23:20 +0100

    Proyecto eclipse

---
+++ Practica1/src/Evolutiva/main.java
@@ -0,0 +1,9 @@
+package Evolutiva;
+
+public class main {
+	
+	public static void main(String args[]) {
+		System.out.println("Practica 1");
+	}
+
+}
---
+++ Practica1/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author javie
+ *
+ */
+module Practica1 {
+}
