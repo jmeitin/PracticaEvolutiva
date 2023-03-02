b4e29a7cacc57098bc132b40aa8d95997dca4ef7
Javier Meitín Moreno <jmeitin@ucm.es>
2023-03-02 12:28:45 +0100

    Deterministico

--- G09P01/src/SelectionAlgorithms/DeterministicTournamentSelection.java
+++ G09P01/src/SelectionAlgorithms/DeterministicTournamentSelection.java
@@ -3,6 +3,8 @@
  */
 package SelectionAlgorithms;
 
+import java.util.Random;
+
 import Exceptions.NotImplementedException;
 import GeneticAlgorithm.Chromosome;
 
@@ -14,8 +16,27 @@ public class DeterministicTournamentSelection extends SelectionAlgorithm {
 
 	@Override
 	public Chromosome[] select(Chromosome[] poblation, int poblation_size) {
-		// TODO Auto-generated method stub
-		throw new NotImplementedException();
+		//CREATE NEW POPULATION
+		Chromosome[] new_population = new Chromosome[poblation_size];
+		Random rand = new Random();
+		int num_people = poblation.length - 1;
+		
+		for (int i = 0; i < poblation_size; i ++) {
+			// SELECT 3 RANDOM SPECIMENS. KEEP THE BEST ONE
+			int a = (int)(rand.nextDouble() * num_people);
+			int b = (int)(rand.nextDouble() * num_people);
+			int c = (int)(rand.nextDouble() * num_people);
+			
+			int best = a;
+			if(poblation[b].getScore() >= poblation[a].getScore() && poblation[b].getScore() >= poblation[c].getScore())
+				best = b;
+			else if (poblation[c].getScore() >= poblation[a].getScore() && poblation[c].getScore() >= poblation[b].getScore())
+				best = c;
+			
+			new_population[i] = poblation[best].getCopy();
+		}
+		
+		return new_population;
 	}
 
 }
