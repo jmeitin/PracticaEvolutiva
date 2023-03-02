199f1d2e849ff81466f79b05357ce27c9a450773
Javier Meitín Moreno <jmeitin@ucm.es>
2023-03-02 20:22:45 +0100

    Truncamiento (Señor ten piedad)

--- G09P01/src/SelectionAlgorithms/TruncationSelection.java
+++ G09P01/src/SelectionAlgorithms/TruncationSelection.java
@@ -3,6 +3,9 @@
  */
 package SelectionAlgorithms;
 
+import java.util.Arrays;
+import java.util.Random;
+
 import Exceptions.NotImplementedException;
 import GeneticAlgorithm.Chromosome;
 
@@ -11,11 +14,36 @@ import GeneticAlgorithm.Chromosome;
  *
  */
 public class TruncationSelection extends SelectionAlgorithm {
-
+	static double trunc = 0.5; // [0.1, 0.5]
+	
 	@Override
 	public Chromosome[] select(Chromosome[] poblation, int poblation_size) {
-		// TODO Auto-generated method stub
-		throw new NotImplementedException();
+		// ORDER POBLATION
+		Arrays.sort(poblation);
+		
+		Chromosome[] new_population = new Chromosome[poblation_size];
+		
+		// 0.3 * 8 eltos = 2.4 ==> i < 2 ==> 2 eltos
+		int index = 0;
+		double num_padres = (trunc * poblation.length);
+		for (int i = 0; i < num_padres; i ++) { 
+			for (int j = 0; j < 1 / trunc; j++) {
+				if(index < poblation_size) {
+					new_population[index] = poblation[i];
+					index += 1;
+				}
+			}
+		}
+		
+		int padre = 0;
+		while (index < poblation_size) {
+			new_population[index] = poblation[padre];
+			index++;
+			if (padre < (int)num_padres) padre++;
+			else padre = 0;
+		}
+		
+		return new_population;
 	}
 
 }
