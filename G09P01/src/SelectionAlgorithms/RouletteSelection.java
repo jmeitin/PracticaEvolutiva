574e10e430e312fc3a85e97370115a14da0351be
Rioni <nrosa01@ucm.es>
2023-03-02 13:12:22 +0100

    Actualizadas seleccion ruleta y estocastica

--- G09P01/src/SelectionAlgorithms/RouletteSelection.java
+++ G09P01/src/SelectionAlgorithms/RouletteSelection.java
@@ -9,23 +9,16 @@ public class RouletteSelection extends SelectionAlgorithm {
 
 	@Override
 	public Chromosome[] select(Chromosome[] poblation, int poblation_size) {
-		double total_sum = 0.0;
-		for (Chromosome cromosoma : poblation) {
-			total_sum += cromosoma.getAccumulatedScore();
-		}
 
 		Chromosome[] new_population = new Chromosome[poblation_size];
-		Random rand = new Random();
 		for (int i = 0; i < poblation_size; i++) {
-			double random_point = rand.nextDouble() * total_sum;
-			double partial_sum = 0.0;
-			for (Chromosome cromosoma : poblation) {
-				partial_sum += cromosoma.getAccumulatedScore();
-				if (partial_sum > random_point) {
-					new_population[i] = cromosoma.getCopy();
-					break;
-				}
-			}
+			double random_point =  Math.random() * Math.random();
+			int pos_super = 0;
+			
+			while(pos_super < poblation_size && random_point > poblation[pos_super].getAccumulatedScore())
+				pos_super++;
+			
+			new_population[i] = poblation[pos_super].getCopy();
 		}
 
 		return new_population;
--- G09P01/src/SelectionAlgorithms/StochasticSelection.java
+++ G09P01/src/SelectionAlgorithms/StochasticSelection.java
@@ -16,43 +16,25 @@ import GeneticAlgorithm.Chromosome;
 public class StochasticSelection extends SelectionAlgorithm {
 
 	@Override
-	public Chromosome[] select(Chromosome[] poblation, int poblation_size) {		
-		Arrays.sort(poblation);
-		
-		//CALCULATE TOTAL FITNESS
-		int fitness_sum = 0;
-		for (int i = 0; i < poblation.length; i++) {
-			fitness_sum += poblation[i].getFitness();
-		}
-		
-		//CALCULATE ACCUMULATED SCORE
-		int accumulated_score = 0;
-		for (int i = 0; i < poblation.length; i++) {
-			// Pi = fitness / fitness_sum
-			poblation[i].setScore(poblation[i].getFitness() / fitness_sum);
-			accumulated_score += poblation[i].getScore();
-			poblation[i].setAccumulatedScore(accumulated_score);
-		}
-		
-		//CREATE NEW POPULATION
+	public Chromosome[] select(Chromosome[] poblation, int poblation_size) {
+
 		Chromosome[] new_population = new Chromosome[poblation_size];
-		Random rand = new Random();
-		double length_segment = 1 / poblation_size;
-		double r = rand.nextDouble() * length_segment;
-		
-		int individuo = 0;
-		for (int i = 0; i < poblation_size; i ++) {
-			double aj = (r + i) / poblation_size; //0.183
+		final double distance = 1.0 / poblation_size;
+		double random_point = Math.random() * distance;
+
+		for (int i = 0; i < poblation_size; i++) {
+
+			int pos_super = 0;
+			while (pos_super < poblation_size && random_point > poblation[pos_super].getAccumulatedScore())
+				pos_super++;
+
+			pos_super = Math.min(pos_super, poblation_size -1); // Clamping
 			
-			// individuo0 = 0.18 < 0.183 < 0.34 = individuo1
-			while (poblation[individuo].getAccumulatedScore() < aj) {
-				individuo++;
-			}
-			new_population[i] = poblation[individuo].getCopy();
+			new_population[i] = poblation[pos_super].getCopy();
+			random_point += distance;
 		}
 		
 		return new_population;
-
 	}
 
 }
