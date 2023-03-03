aacaa6016d32838e57fcdf71d981b6acaf1d1ffc
Rioni <nrosa01@ucm.es>
2023-03-03 13:26:04 +0100

    Codigo refactorizado, arquitectura mejorada

--- G09P01/src/CrossAlgorithms/ArithmeticCross.java
+++ G09P01/src/CrossAlgorithms/ArithmeticCross.java
@@ -13,57 +13,23 @@ public class ArithmeticCross extends CrossAlgorithm {
 	final static double alpha = 0.5;
 	
 	@Override
-	public Chromosome[] cross(Chromosome[] poblation, int poblation_size, double cross_chance, int num_points) {
-		//System.out.print("ARITMETICO");
+	protected void cross(Chromosome parent_a, Chromosome parent_b) {
+		//CASTING FOR 4B------------------------------------------------------------------------------------------
+		ChromosomeP1F4b childA_4b = (ChromosomeP1F4b)parent_a;
+		ChromosomeP1F4b childB_4b = (ChromosomeP1F4b)parent_b;
 		
-		Chromosome[] new_population = new Chromosome[poblation_size];
-		Random rand = new Random();
+		int num_genes = childA_4b.getLenght();
+		int num_alleles = childA_4b.getAlleleLength();
 		
-		int parent_selected = -1; // 1st parent hasn't been selected yet
-		for (int i = 0; i < poblation_size; i++) {
-			double chance = rand.nextDouble(); // [0, 1]
-			
-			//CROSS HAPPENS
-			if(chance <= cross_chance) {
-				// THERE IS NO 1st PARENT
-				if (parent_selected == - 1) { 
-					parent_selected = i;
-					new_population[parent_selected] = poblation[parent_selected].getCopy(); //default value
-				}
-				//SELECT 2nd PARENT
-				else { 
-					//CASTING FOR 4B------------------------------------------------------------------------------------------
-					ChromosomeP1F4b childA_4b = (ChromosomeP1F4b)poblation[parent_selected].getCopy();
-					ChromosomeP1F4b childB_4b = (ChromosomeP1F4b)poblation[i].getCopy();
-					
-					int num_genes = childA_4b.getLenght();
-					int num_alleles = childA_4b.getAlleleLength();
-					
-					for (int g = 0; g < num_genes; g++) {
-						for (int a = 0; a < num_alleles; a++) {
-							double a1 = childA_4b.getGene(g).getAllele(a);
-							double a2 = childB_4b.getGene(g).getAllele(a);
-							
-							//hi = αp1i + (1-α)p2i
-							childA_4b.getGene(g).setAllele(a, alpha * a1 + (alpha -1) * a2);
-							childB_4b.getGene(g).setAllele(a, (alpha -1) * a1 + alpha * a2);
-						}
-					}
-					//UPDATE NEW POPULATION
-					new_population[parent_selected] = childA_4b;
-					new_population[i] = childB_4b;	
-					parent_selected = -1; // select 1st parent for new couple next
-				}
+		for (int g = 0; g < num_genes; g++) {
+			for (int a = 0; a < num_alleles; a++) {
+				double a1 = childA_4b.getGene(g).getAllele(a);
+				double a2 = childB_4b.getGene(g).getAllele(a);
 				
+				//hi = αp1i + (1-α)p2i
+				childA_4b.getGene(g).setAllele(a, alpha * a1 + (alpha -1) * a2);
+				childB_4b.getGene(g).setAllele(a, (alpha -1) * a1 + alpha * a2);
 			}
-			
-			//CROSS DOESN'T HAPPEN
-			else {
-				new_population[i] = poblation[i].getCopy();
-			}
-			
 		}
-
-		return new_population;		
 	}
 }
--- G09P01/src/CrossAlgorithms/OnePointCross.java
+++ G09P01/src/CrossAlgorithms/OnePointCross.java
@@ -7,62 +7,24 @@ import GeneticAlgorithm.Chromosome;
 import GeneticAlgorithm.Gene;
 
 public class OnePointCross extends CrossAlgorithm {
-
 	@Override
-	public Chromosome[] cross(Chromosome[] poblation, int poblation_size, double cross_chance, int num_points) {
-		//x(v) = xMin + bin2dec(v) * (xMax - xMin) / (2^lengthChromosome - 1)
+	protected void cross(Chromosome childA, Chromosome childB) {
+		int length = childA.getLenght();
+		int alleleLength = childA.getAlleleLength();
+		// Calculate Point in [1, L-1] that delimits 2 portions
+		int allelePoint = calculateNextPoint(rand, 1 * alleleLength, (length - 1) * alleleLength);
+		// 4 genes, 2 bits each ==> 01 23 45 67
+		// allelePoint = 5 
+		// genes = 5 / 2bits = gene2
+		int gene = allelePoint / alleleLength;
+		// allele = 5 % 2bits = allele1
+		int allele = allelePoint % alleleLength;
 		
-		Chromosome[] new_population = new Chromosome[poblation_size];
-		Random rand = new Random();
-		
-		int parent_selected = -1; // 1st parent hasn't been selected yet
-		for (int i = 0; i < poblation_size; i++) {
-			double chance = rand.nextDouble(); // [0, 1]
-			
-			//CROSS HAPPENS
-			if(chance <= cross_chance) {
-				// THERE IS NO 1st PARENT
-				if (parent_selected == - 1) { 
-					parent_selected = i;
-					new_population[parent_selected] = poblation[parent_selected].getCopy(); //default value
-				}
-				//SELECT 2nd PARENT
-				else { 
-					Chromosome childA = poblation[parent_selected].getCopy();
-					Chromosome childB = poblation[i].getCopy();
-					
-					int length = childA.getLenght();
-					int alleleLength = childA.getAlleleLength();
-					// Calculate Point in [1, L-1] that delimits 2 portions
-					int allelePoint = calculateNextPoint(rand, 1 * alleleLength, (length - 1) * alleleLength);
-					// 4 genes, 2 bits each ==> 01 23 45 67
-					// allelePoint = 5 
-					// genes = 5 / 2bits = gene2
-					int gene = allelePoint / alleleLength;
-					// allele = 5 % 2bits = allele1
-					int allele = allelePoint % alleleLength;
-					
-					//CROSS GENES ON 2ND PORTION (1st PORTION STAYS THE SAME)					
-					for (int g = gene; g < length; g++) {
-						childA.swapAllelesInGene(childB, g, allele, rand, 2); // Cross chance is (200%)
-						if (g == gene + 1)
-							allele = 0; //will exchange all alleles in gene
-					}
-					//UPDATE NEW POPULATION
-					new_population[parent_selected] = childA;
-					new_population[i] = childB;	
-					parent_selected = -1; // select 1st parent for new couple next
-				}
-				
-			}
-			
-			//CROSS DOESN'T HAPPEN
-			else {
-				new_population[i] = poblation[i].getCopy();
-			}
-			
+		//CROSS GENES ON 2ND PORTION (1st PORTION STAYS THE SAME)					
+		for (int g = gene; g < length; g++) {
+			childA.swapAllelesInGene(childB, g, allele, rand, 2); // Cross chance is (200%)
+			if (g == gene + 1)
+				allele = 0; //will exchange all alleles in gene
 		}
-
-		return new_population;		
 	}
 }
