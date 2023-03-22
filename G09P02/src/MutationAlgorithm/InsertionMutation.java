41a1892d7cbc1643a122fc02722b5dca273deffd
Javier Meitín Moreno <jmeitin@ucm.es>
2023-03-22 14:56:57 +0100

    Mutacion por insercion (Señor ten piedad)

--- G09P02/src/MutationAlgorithm/InsertionMutation.java
+++ G09P02/src/MutationAlgorithm/InsertionMutation.java
@@ -3,6 +3,7 @@ package MutationAlgorithm;
 import java.util.Arrays;
 
 import Chromosomes.Chromosome;
+import Chromosomes.ChromosomeP2;
 import Chromosomes.Gene;
 
 //A NIVEL DE GEN
@@ -14,35 +15,86 @@ public class InsertionMutation extends MutationAlgorithm{
 	@Override
 	public Chromosome[] mutate(Chromosome[] poblation, int poblation_size, double mutation_chance){
 		if (inserted_num > poblation_size) 
-			inserted_num = 1;
+			inserted_num = 1;		
 		
 		int num_genes = poblation[0].getNumOfGenes();
-		int [] can_insert = new int[inserted_num]; // indicates if allele will be inserted
+		int [] move_pos = new int[inserted_num]; // indicates genes that will be moves
+		int [] insert_pos = new int[inserted_num];
 		Chromosome[] new_population = new Chromosome[poblation_size];
 		
 		for (int i = 0; i < poblation_size; i++) {			
-			//CALCULATE INSERTION POINTS-------------------------------
+			//CALCULATE RANDOM INSERTION POINTS & NUMBERS TO INSERT-------------------------------
 			for(int j = 0; j < inserted_num; j++) {
-				can_insert[j] = (int)(rand.nextDouble() * (num_genes - 1));
+				move_pos[j] = (int)(rand.nextDouble() * (num_genes - 1));
+				insert_pos[j] = (int)(rand.nextDouble() * (num_genes - 1));
 			}
-			Arrays.sort(can_insert); //order array
+			Arrays.sort(move_pos); //order array <
+			Arrays.sort(insert_pos); //order array <
 			
 			//MUTATE CHROMOSOME---------------------------------------------------
-			new_population[i] = poblation[i].getCopy();
+			ChromosomeP2 chromosome = (ChromosomeP2)poblation[i].getCopy();
 			
+			int[] genes = chromosome.getGenesCopy();
+			int[] new_genes = new int[num_genes];
 			int i_insert = 0;
-			//GENES IN CHROMOSOME
-			for (int g = 0; g < num_genes; g ++) {				
-				if(can_insert[i_insert] == g) {
-					int other = g + (int)(rand.nextDouble() * (num_genes - g - 1)); //another gene in [g, L-1]
-					new_population[i].displaceGenes(g, other);
-				}					
-				
-				i_insert++;
+			// INSERT NUMBERS FIRST---------------------------------------------------
+			for(int g = 0; g < num_genes; g++) {
+				// I want to insert number here
+				if(insert_pos[i_insert] == g) {
+					//Pos where number that wants to be inserted is found
+					int old_pos = move_pos[i_insert];
+					//Move gene
+					new_genes[g] = genes[old_pos];
+
+					//IN CASE THAT A NUMBER (move_pos or insert_pos) WAS SELECTED MULTIPLE TIMES
+					if (i_insert + 1 < inserted_num && 
+							(move_pos[i_insert] == move_pos[i_insert + 1] || insert_pos[i_insert] == insert_pos[i_insert + 1])) {
+						while (i_insert + 1 < inserted_num && 
+								(move_pos[i_insert] == move_pos[i_insert + 1] || insert_pos[i_insert] == insert_pos[i_insert + 1]))
+							i_insert++;
+					}
+					//MOVE TO NEXT INSERT NUMBER
+					else { 
+						i_insert++;
+					}
+				}
+				// NON VALID DISTANCE
+				else new_genes[g] = -1; 
 			}
+
+			// DISPLACE NUMBERS WITH NON VALID DISTANCE-----------------------------------------------
+			int old_pos = 0;
+			int new_pos = 0;
+			while (new_pos < num_genes && old_pos < num_genes) {
+				
+				// THIS NUMBER HAS ALREADY BEEN INSERTED ELSEWHERE
+				while(old_pos < num_genes && numberExistsInArray(move_pos, old_pos)) {
+					old_pos++;
+				}
+				// THIS IS AN INSERTED NUMBER (For loop above)
+				while(new_pos < num_genes && new_genes[new_pos] != -1) {
+					new_pos++;
+				}
+				// WE HAVE A VALID move_pos & insert_pos
+				if (new_pos < num_genes && old_pos < num_genes) {
+					new_genes[new_pos] = genes[old_pos];
+					new_pos++;
+					old_pos++;					
+				}
+			}		
+
+			new_population[i] = chromosome;
 		}
 
 		return new_population;	
+	}
+	
+	private boolean numberExistsInArray(int[] positions, int value) {
+		int i = 0; 
+		while (i < positions.length && positions[i] != value)
+			i++;
+		//returns true if value was found in array
+		return i != positions.length; 
 	}
 	
 	public void setInsertedNum(int n) {
