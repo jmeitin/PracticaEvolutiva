89e7f406af87314c092ed514a1ad991b0c6ac414
Javier Meitín Moreno <jmeitin@ucm.es>
2023-03-24 13:00:32 +0100

    No se me ocurre nada

---
+++ G09P02/src/CrossAlgorithms/P2/OriginalCross.java
@@ -0,0 +1,14 @@
+package CrossAlgorithms.P2;
+
+import Chromosomes.ChromosomeP2;
+
+/*
+
+ * */
+public class OriginalCross extends CrossAlgorithmsP2 {
+
+	@Override
+	protected void cross(ChromosomeP2 first_child, ChromosomeP2 second_child) {
+		
+	}
+}
