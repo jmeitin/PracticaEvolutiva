44c07f35bc55a18755be9cd4f021c6df87056450
Rioni <nrosa01@ucm.es>
2023-03-25 05:04:03 +0100

    ERXCross ahora si va bien

--- G09P02/src/CrossAlgorithms/P2/ERXCross.java
+++ G09P02/src/CrossAlgorithms/P2/ERXCross.java
@@ -1,6 +1,7 @@
 package CrossAlgorithms.P2;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -25,78 +26,64 @@ public class ERXCross extends CrossAlgorithmsP2 {
 		second_child.setGenes(second_child_new_genes);
 	}
 
-	// Reconstruct a Hamiltonian path from two children and a table of adjacent vertices
-	// Side note: We now we are building a Hamiltonian path because we know the graph is connected, has n vertex and we visit each one of them only once
-	// We use backtracking to try every posible path. But somehow this still doesn't work and everyonce in a while gets stuck in a infinite loop
+	// Reconstruct a Hamiltonian path from two children and a table of adjacent
+	// vertices
+	// Side note: We now we are building a Hamiltonian path because we know the
+	// graph is connected, has n vertex and we visit each one of them only once
+	// We use backtracking to try every posible path. But somehow this still doesn't
+	// work and everyonce in a while gets stuck in a infinite loop
 	protected static int[] reconstructFromTable(int[] child1, int[] child2, HashMap<Integer, List<Integer>> table) {
-	    int[] result = new int[child1.length];
-	    result[0] = child1[0];
-	    Set<Integer> result_set = new HashSet<Integer>();
-	    Set<Integer> visited_neighbours = new HashSet<Integer>();
-	    result_set.add(child1[0]);
+		int[] result = new int[child1.length];
+		result[0] = child1[0];
+		Set<Integer> result_set = new HashSet<Integer>();
+		result_set.add(child1[0]);
 
-	    // Use helper function to recursively explore paths until a valid Hamiltonian path is found
-	    if (reconstructFromTableHelper(result, result_set, visited_neighbours, table, 1)) {
-	        return result;
-	    } else {
-	        // If no path was found, retry with a different random starting vertex
-	        return reconstructFromTable(child1, child2, table);
-	    }
+		// Use helper function to recursively explore paths until a valid Hamiltonian
+		// path is found
+		if (reconstructFromTableHelper(result, result_set, table, 1)) {
+			return result;
+		} else {
+			// If no path was found, retry with a different random starting vertex
+			return reconstructFromTable(child1, child2, table);
+		}
 	}
 
 	// Recursive helper function to explore potential paths for Hamiltonian paths
 	protected static boolean reconstructFromTableHelper(int[] result, Set<Integer> result_set,
-	        Set<Integer> visited_neighbours, HashMap<Integer, List<Integer>> table, int i) {
+			HashMap<Integer, List<Integer>> table, int i) {
 
-	    // Base case: if all vertices have been added to the path, return true
-	    if (i == result.length) {
-	        return true;
-	    }
+		// Base case: if all vertices have been added to the path, return true
+		if (i == result.length)
+			return true;
 
-	    // Get the neighbors of the last added vertex and choose the least connected neighbor
-	    List<Integer> neighbors = table.get(result[i - 1]);
-	    int least_connected_neighbour = -1;
-	    for (int neighbour : neighbors) {
-	        if (visited_neighbours.contains(neighbour)) {
-	            continue;
-	        }
-	        visited_neighbours.add(neighbour);
-	        if (result_set.contains(neighbour)) {
-	            continue;
-	        }
+		// Get the neighbors of the last added vertex and choose the least connected
+		// neighbor
+		List<Integer> neighbors = table.get(result[i - 1]);
 
-	        // Check if this neighbor is less connected than the current least connected neighbor
-	        final int neighbour_size = table.get(neighbour).size();
-	        final int least_connected_neighbour_size = least_connected_neighbour != -1
-	                ? table.get(least_connected_neighbour).size()
-	                : Integer.MAX_VALUE;
+		// Sort neighbours, so we explore first the ones with less connections
+		Collections.sort(neighbors, (v1, v2) -> Integer.compare(table.get(v1).size(), table.get(v2).size()));
 
-	        if (neighbour_size == least_connected_neighbour_size) {
-	            least_connected_neighbour = neighbour;
-	        } else if (neighbour_size < least_connected_neighbour_size) {
-	            least_connected_neighbour = neighbour;
-	        }
+		for (int neighbour : neighbors) {
+			if (result_set.contains(neighbour))
+				continue;
+			
+			// Add the least connected neighbor to the path and continue recursively
+			// As neighbours are sorted we can just try them in order
+			// exploring paths
+			result[i] = neighbour;
+			result_set.add(neighbour);
 
-	        // Add the least connected neighbor to the path and continue recursively exploring paths
-	        result[i] = least_connected_neighbour;
-	        result_set.add(least_connected_neighbour);
-	        visited_neighbours.clear();
+			// If a valid Hamiltonian path is found, return true
+			if (reconstructFromTableHelper(result, result_set, table, i + 1))
+				return true;
+			else 
+				// If a valid path is not found, backtrack and try a different neighbor
+				result_set.remove(neighbour);
+		}
 
-	        // If a valid Hamiltonian path is found, return true
-	        if (reconstructFromTableHelper(result, result_set, visited_neighbours, table, i + 1)) {
-	            return true;
-	        } else {
-	            // If a valid path is not found, backtrack and try a different neighbor
-	            result_set.remove(least_connected_neighbour);
-	            visited_neighbours.remove(neighbour);
-	            continue;
-	        }
-	    }
-
-	    // If no valid path is found, return false
-	    return false;
+		// If no valid path is found, return false
+		return false;
 	}
-
 
 	// Bidirectional module function
 	// module (1, 0, 2) // Output: 1
